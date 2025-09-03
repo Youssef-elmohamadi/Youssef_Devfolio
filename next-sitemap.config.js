5420ccc33cfcb980932541aa890e18f511f6b2b1
Youssef-elmohamadi <youssefelmohammadi24@gmail.com>
2025-09-03 21:02:17 +0300

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://the-forge-one.vercel.app", // رابط موقعك الأساسي
+  generateRobotsTxt: true, // هيعمل robots.txt كمان
+  sitemapSize: 5000, // عدد اللينكات في ملف sitemap واحد (تقدر تغير الرقم)
+};
