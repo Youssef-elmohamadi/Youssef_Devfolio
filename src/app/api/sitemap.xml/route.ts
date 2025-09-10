44419ec885675fa5a26bda5b57857f61117a2c08
Youssef-elmohamadi <youssefelmohammadi24@gmail.com>
2025-09-10 18:27:44 +0300

    add Vercel json

--- src/app/api/sitemap.xml/route.ts
+++ src/app/api/sitemap.xml/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { readFileSync } from "fs";
 import { join } from "path";
 
+export const dynamic = "force-dynamic"; // 🟢 يخلي Next.js يستنى runtime
+
 export async function GET() {
   const filePath = join(process.cwd(), "public", "sitemap.xml");
   const xml = readFileSync(filePath, "utf8");
